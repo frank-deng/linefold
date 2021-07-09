29de32289274cf820efe486f194aa34dc94bde8a
frank-deng <13382084679@163.com>
2021-07-09 19:20:27 +0800

    Overwidth group splitting activated

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ const HEAD_CHAR={
 let ctx=null;
 try{
     const canvas=document.createElement('canvas');
-    canvas.width=canvas.height=10;
+    canvas.width=canvas.height=16;
     ctx=canvas.getContext('2d');
 }catch(e){
-    console.error('Failed to initialize canvas',e);
+    console.info('HTML5 canvas not available, default canvas-based string length measuring will not work');
 }
 
 function matchedStr(str,matcherList=[]){
@@ -92,7 +92,7 @@ function matchedStr(str,matcherList=[]){
     }
     return str[0];
 }
-export function splitStr(str0, matcherList=[]){
+export function group(str0, matcherList=[]){
     let result=[], str=str0.slice(), prevCharProc=null;
     while(str.length){
         //使用给定的正则进行匹配，如果匹配且匹配的位置在剩余字符串头部，则单独成为一组或整体添加到上一组末尾
@@ -113,9 +113,48 @@ export function splitStr(str0, matcherList=[]){
     }
     return result;
 }
-export default function(text, textLengthMeasure){
+export function split(str,maxWidth,measureFunc){
+    let result=[], start=0, end=1, len=str.length;
+    while(start<len && end<=len){
+        let substr=str.slice(start,end);
+        if(measureFunc(substr)<=maxWidth){
+            end++;
+        }else{
+            result.push(str.slice(start,end-1));
+            start=end-1;
+        }
+    }
+    if(start<len){
+        result.push(str.slice(start,len));
+    }
+    return result.length ? result : [str];
+}
+export default function(text, maxWidth, textLengthMeasure, extraRules=[]){
+    //默认的测量函数，使用HTML5 canvas
+    let measureFunc=function(str){
+        return ctx.measureText(str).width;
+    };
+
     //如果textLengthMeasure是字符串，则作为canvas模式的字体样式使用
     if(ctx && 'string'===typeof(textLengthMeasure)){
         ctx.font=textLengthMeasure;
+    }else if('function'==typeof(textLengthMeasure)){
+        //给定的测量函数
+        measureFunc=textLengthMeasure;
+    }else{
+        throw new TypeError('Second parameter must be a function for measuring string length');
+    }
+
+    //对字符进行分组
+    let groups=group(text,extraRules);
+
+    //找出超长的分组进行拆分
+    //一旦找到超长的组，拆分过程将比较耗时，因此需要避免文本中出现过长的单词
+    for(let i=groups.length-1; i>=0; i--){
+        let group=groups[i];
+        if(measureFunc(group) <= maxWidth){
+            continue;
+        }
+        groups.splice(i,1,...split(group,maxWidth,measureFunc));
     }
 }
--- test.js
+++ test.js
@@ -3,22 +3,45 @@ const assert = require('assert');
 
 describe('Grouping test',function(){
     it('Grouping test 1',function(){
-        assert.deepStrictEqual(Linefold.splitStr('Mocha Test'),[
+        assert.deepStrictEqual(Linefold.group('Mocha Test'),[
             'Mocha ',
             'Test'
         ]);
-        assert.deepStrictEqual(Linefold.splitStr('Mocha \tTest\t'),[
+        assert.deepStrictEqual(Linefold.group('Mocha \tTest\t'),[
             'Mocha \t',
             'Test\t'
         ]);
     });
     it('Chinese test',function(){
-        assert.deepStrictEqual(Linefold.splitStr('HTML5的canvas元素使用JavaScript在网页上绘制图像，画布是一个矩形区域。'),[
+        assert.deepStrictEqual(Linefold.group('HTML5的canvas元素使用JavaScript在网页上绘制图像，画布是一个矩形区域。'),[
             `HTML5`,
             `的`,
             `canvas`,
             `元`,`素`,`使`,`用`,`JavaScript`,`在`,`网`,`页`,`上`,
             `绘`,`制`,`图`,`像，`,`画`,`布`,`是`,`一`,`个`,`矩`,`形`,`区`,`域。`
+        ]);
+    });
+});
+describe('Overwidth test',function(){
+    it('Split text',function(){
+        assert.deepStrictEqual(Linefold.split('ABCDEFGH',8*4,(str)=>(str.length*8)),[
+            'ABCD',
+            'EFGH'
+        ]);
+        assert.deepStrictEqual(Linefold.split('ABCDEFGHI',8*4,(str)=>(str.length*8)),[
+            'ABCD',
+            'EFGH',
+            'I'
+        ]);
+        assert.deepStrictEqual(Linefold.split('ABCDEFGHI',36,(str)=>(str.length*8)),[
+            'ABCD',
+            'EFGH',
+            'I'
+        ]);
+        assert.deepStrictEqual(Linefold.split('ABCDEFGHIJ',36,(str)=>(str.length*8)),[
+            'ABCD',
+            'EFGH',
+            'IJ'
         ]);
     });
 });
