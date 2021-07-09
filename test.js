28fd6afe354da7de1aaab8b1aa9abcd6e700c032
frank-deng <13382084679@163.com>
2021-07-09 18:06:21 +0800

    Test case updated

--- test.js
+++ test.js
@@ -12,4 +12,13 @@ describe('Grouping test',function(){
             'Test\t'
         ]);
     });
+    it('Chinese test',function(){
+        assert.deepStrictEqual(Linefold.splitStr('HTML5的canvas元素使用JavaScript在网页上绘制图像，画布是一个矩形区域。'),[
+            `HTML5`,
+            `的`,
+            `canvas`,
+            `元`,`素`,`使`,`用`,`JavaScript`,`在`,`网`,`页`,`上`,
+            `绘`,`制`,`图`,`像，`,`画`,`布`,`是`,`一`,`个`,`矩`,`形`,`区`,`域。`
+        ]);
+    });
 });
