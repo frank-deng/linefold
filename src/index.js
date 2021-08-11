05c5d5d9d53e2de3f4b01c3879311c3bf3b5b3ac
frank-deng <13382084679@163.com>
2021-08-11 20:22:56 +0800

    Updated test page

--- src/index.js
+++ src/index.js
@@ -1,88 +1,45 @@
-
-var LANG_DATA={
-    en:{
-        title:'Line Folding Test Page',
-        width:'Width',
-        fontSize:'Font Size',
-        font:'Font',
-        bold:'Bold',
-        italic:'Italic',
-        text:'Text',
-        effect:'Effect'
-    },
-    ja:{
-        title:'テキストの改行のテストページ',
-        width:'幅',
-        fontSize:'サイズ',
-        font:'フォント',
-        bold:'太字',
-        italic:'斜体',
-        text:'テキスト',
-        effect:'効果'
-    },
-    zh:{
-        title:'文本换行算法测试页面',
-        width:'宽度',
-        fontSize:'字体大小',
-        font:'字体',
-        bold:'加粗',
-        italic:'倾斜',
-        text:'正文',
-        effect:'效果'
-    }
-};
-//获取当前使用的语言优先级
-var lang=[], langFallback='en';
+import Lang from './lang.js';
 try{
-    var langRaw=window.navigator.languages || [window.navigator.language];
-    //从URL中获取语言，覆盖浏览器的语言设置
-    var langMatch=/language=([A-Za-z0-9_]+)/.exec(window.location.search);
-    if(langMatch && langMatch[1]){
-        langRaw=[langMatch];
-    }
-    //根据本页面可用的语言获取语言优先级
-    for(var i=0;i<langRaw.length;i++){
-        var langRawItem=langRaw[i];
-        for(var langAvail in LANG_DATA){
-            if(-1!=langRawItem.indexOf(langAvail)){
-                lang.push(langAvail);
-            }
+    const lang=new Lang({
+        en:{
+            title:'Line Folding Test Page',
+            width:'Width',
+            fontSize:'Font Size',
+            font:'Font',
+            bold:'Bold',
+            italic:'Italic',
+            text:'Text',
+            effect:'Effect'
+        },
+        ja:{
+            title:'テキストの改行のテストページ',
+            width:'幅',
+            fontSize:'サイズ',
+            font:'フォント',
+            bold:'太字',
+            italic:'斜体',
+            text:'テキスト',
+            effect:'効果'
+        },
+        zh:{
+            title:'文本换行算法测试页面',
+            width:'宽度',
+            fontSize:'字体大小',
+            font:'字体',
+            bold:'加粗',
+            italic:'倾斜',
+            text:'正文',
+            effect:'效果'
         }
+    });
+    document.title=lang.get('title');
+    let domTranslateList=document.querySelectorAll('[translate]');
+    for(let i=0; i<domTranslateList.length; i++){
+        let domTranslate=domTranslateList[i];
+        domTranslate.innerHTML=lang.get(domTranslate.getAttribute('translate'));
     }
 }catch(e){
-    console.error('Failed to get language info',e);
-}
-
-//根据当前的语言优先级和给定的key获取对应的文本
-function t(key){
-    try{
-        var langItem, langData, i;
-        for(i=0; i<lang.length; i++){
-            langItem=lang[i];
-            langData=LANG_DATA[langItem];
-            if(langData && langData[key]){
-                return langData[key];
-            }
-        }
-        langData=LANG_DATA[langFallback];
-        if(langData[key]){
-            return langData[key];
-        }
-    }catch(e){
-        console.error(e);
-    }
-    return key;
-}
-
-//初始化各个多语言文本
-document.title=t('title');
-var domTranslateList=document.querySelectorAll('[translate]');
-for(var i=0;i<domTranslateList.length;i++){
-    var domTranslate=domTranslateList[i];
-    var translatedText=t(domTranslate.getAttribute('translate'));
-    if(translatedText){
-        domTranslate.innerHTML=translatedText;
-    }
+    console.error('Failed to load translation',e);
 }
 
 var canvas=document.getElementById('testCanvas'),
